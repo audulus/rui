cf1c6739a096855a9bf44efc599c601e90647171
Taylor Holliday <taylor@audulus.com>
2022-01-18 20:28:44 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,97 @@
+
+enum Stack {
+    HStack,
+    VStack
+}
+
+enum Command {
+    Button(String),
+    Text(String),
+    Begin(Stack),
+    End
+}
+
+enum State {
+    Draw,
+    Process,
+}
+
+struct Gui {
+    commands: Vec<Command>,
+    state: State
+}
+
+impl Gui {
+
+    pub fn new() -> Self {
+        Self {
+            commands: vec![],
+            state: State::Draw
+        }
+    }
+
+    pub fn button(&mut self, name: &str) -> bool {
+        match self.state {
+            State::Draw => {
+                self.commands.push(Command::Button(String::from(name)));
+                false
+            }
+            State::Process => {
+                false
+            }
+        }
+    }
+
+    fn text(&mut self, name: &str) {
+        match self.state {
+            State::Draw => {
+                self.commands.push(Command::Button(String::from(name)));
+            }
+            State::Process => { }
+        }
+    }
+
+    fn begin_hstack(&mut self) {
+        match self.state {
+            State::Draw => {
+                self.commands.push(Command::Begin(Stack::HStack));
+            }
+            State::Process => { }
+        }
+    }
+
+    fn end_hstack(&mut self) {
+        match self.state {
+            State::Draw => {
+                self.commands.push(Command::Begin(Stack::HStack));
+            }
+            State::Process => { }
+        }
+    }
+
+    fn hstack<F : FnOnce(&mut Gui)>(&mut self, f: F) {
+        self.begin_hstack();
+        f(self);
+        self.end_hstack();
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn describe_gui() {
+        
+        let mut gui = Gui::new();
+
+        gui.hstack(|gui|{
+            if gui.button("click me!") {
+                println!("clicked!")
+            }
+            gui.text("Hello world!");
+        })
     }
+    
 }
