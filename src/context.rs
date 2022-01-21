84d2bac056e3e466f599d988e0cb97b08c41f48c
Taylor Holliday <taylor@audulus.com>
2022-01-21 14:35:47 -0800

    Update context.rs

--- src/context.rs
+++ src/context.rs
@@ -8,6 +8,25 @@ struct ViewID {
     len: usize
 }
 
+impl ViewID {
+    fn child(&self, index: u16) -> Self {
+        let mut c = *self;
+        assert!(c.len < 32);
+        c.path[c.len] = index;
+        c.len += 1;
+        c
+    }
+}
+
 struct Context {
     state_map: HashMap<ViewID, Box<dyn AnyState>>
+}
+
+impl Context {
+
+    fn new() -> Self {
+        Self {
+            state_map: HashMap::new()
+        }
+    }
 }
