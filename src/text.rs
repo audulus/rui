9f5b228644210ad2697e3ce8ed06026d308cc9e0
Taylor Holliday <taylor@audulus.com>
2022-01-21 22:27:35 -0800

    Update text.rs

--- src/text.rs
+++ src/text.rs
@@ -11,6 +11,16 @@ impl View for Text {
     fn process(&self, _event: &Event, _id: ViewID, _cx: &mut Context) {}
 }
 
+impl Text {
+    fn layout(&self, id: ViewID, cx: &mut Context) -> LocalSize {
+        // XXX: obviously need to use vger to computer text size
+        let size = LocalSize::new(self.text.len() as f32 * 10.0, 10.0);
+        cx.layout.insert(id, LayoutBox{ rect: LocalRect::new(LocalPoint::zero(), size),
+                                        offset: LocalOffset::zero() });
+        size
+    }
+}
+
 pub fn text(name: &str) -> Text {
     Text {
         text: String::from(name),
