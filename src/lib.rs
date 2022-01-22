84787c3ce5f19154d1665367d85f71ed12a0edaa
Taylor Holliday <taylor@audulus.com>
2022-01-22 08:05:50 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -74,15 +74,15 @@ mod tests {
         state(start, |count: State<usize>| {
             let count2 = count.clone();
             let value_string = format!("value: {:?}", *count.get());
-            stack3(
-                text(value_string.as_str()),
+            stack! {
+                text(value_string.as_str());
                 button("increment", move || {
                     *count.get() += 1;
-                }),
+                });
                 button("decrement", move || {
                     *count2.get() -= 1;
-                }),
-            )
+                })
+            }
         })
     }
 
