9b226105e125bb28970c0a1413cb6da2ece55fce
Taylor Holliday <taylor@audulus.com>
2022-02-09 15:14:26 -0800

    Update basic.rs

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,6 +4,37 @@ use winit::{
    window::WindowBuilder,
 };
 
+use wgpu::*;
+//use vger::*;
+//use vger::color::Color;
+//use vger::defs::*;
+use futures::executor::block_on;
+
+async fn setup() -> (wgpu::Device, wgpu::Queue) {
+    let backend = wgpu::Backends::all();
+    let instance = wgpu::Instance::new(backend);
+
+    let adapter = wgpu::util::initialize_adapter_from_env_or_default(&instance, backend, None)
+        .await
+        .expect("No suitable GPU adapters found on the system!");
+
+    let adapter_info = adapter.get_info();
+    println!("Using {} ({:?})", adapter_info.name, adapter_info.backend);
+
+    let trace_dir = std::env::var("WGPU_TRACE");
+    adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::default(),
+                limits: wgpu::Limits::default(),
+            },
+            trace_dir.ok().as_ref().map(std::path::Path::new),
+        )
+        .await
+        .expect("Unable to find a suitable GPU adapter!")
+}
+
 fn main() {
    let event_loop = EventLoop::new();
    let window = WindowBuilder::new().build(&event_loop).unwrap();
