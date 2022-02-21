e035f6b69f43fc4e590818615a9a0f658bfbfce9
Taylor Holliday <taylor@audulus.com>
2022-02-21 14:02:11 -0800

    Update shapes.rs

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -2,7 +2,7 @@ use rui::*;
 
 fn main() {
     rui(vstack! {
-        circle().color(RED_HIGHLIGHT);
-        rectangle(5.0).color(AZURE_HIGHLIGHT)
+        circle().color(RED_HIGHLIGHT).padding();
+        rectangle(5.0).color(AZURE_HIGHLIGHT).padding()
     });
 }
