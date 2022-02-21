0432ace6de1a56c773a6c63e94ef7af110bf2c09
Taylor Holliday <taylor@audulus.com>
2022-02-21 14:43:15 -0800

    Update counter.rs

--- examples/counter.rs
+++ examples/counter.rs
@@ -3,10 +3,12 @@ use rui::*;
 fn main() {
     rui(state(1, |count: State<usize>| {
         vstack! {
-            text(format!("{:?}", *count.get()).as_str()).padding(Auto);
+            text(&format!("{:?}", *count.get()))
+                .padding(Auto);
             button("increment", move || {
                 *count.get() += 1;
-            }).padding(Auto)
+            })
+                .padding(Auto)
         }
     }));
 }
