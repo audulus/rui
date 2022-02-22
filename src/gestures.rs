87cf680452957d6351a8b464281f41755c026fd0
Taylor Holliday <taylor@audulus.com>
2022-02-21 18:36:48 -0800

    Create gestures.rs

---
+++ src/gestures.rs
@@ -0,0 +1,40 @@
+
+pub use crate::*;
+
+pub struct Tap<V: View> {
+    child: V,
+    func: Box<dyn Fn()>,
+}
+
+impl<V> View for Tap<V>
+where
+    V: View,
+{
+    fn print(&self, id: ViewID, cx: &mut Context) {
+        println!("Tap {{");
+        (self.child).print(id.child(0), cx);
+        println!("}}");
+    }
+
+    fn process(&self, event: &Event, id: ViewID, cx: &mut Context) {
+        
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.draw(id.child(0), cx, vger)
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        self.child.layout(id.child(0), sz, cx, vger)
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        self.child.hittest(id.child(0), pt, cx, vger)
+    }
+}
