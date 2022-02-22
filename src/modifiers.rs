b9e5644fa64818dcdaf6a8cd315e9857199e4214
Taylor Holliday <taylor@audulus.com>
2022-02-22 09:12:25 -0800

    Update modifiers.rs

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -4,6 +4,7 @@ pub trait Modifiers: View + Sized {
     fn padding(self, param: impl Into<PaddingParam>) -> Padding<Self>;
     fn tap<F: Fn() + 'static>(self, f: F) -> Tap<Self>;
     fn background<BG: View + 'static>(self, background: BG) -> Background<Self, BG>;
+    fn geom<F: Fn(WorldRect) + 'static>(self, f: F) -> Geom<Self>;
 }
 
 impl <V: View + 'static> Modifiers for V {
@@ -15,5 +16,8 @@ impl <V: View + 'static> Modifiers for V {
     }
     fn background<BG: View + 'static>(self, background: BG) -> Background<Self, BG> {
         Background::new(self, background)
+    }
+    fn geom<F: Fn(WorldRect) + 'static>(self, f: F) -> Geom<Self> {
+        Geom::new(self, f)
     }
 }
