7bb098ad86492198f25fd6890a1aa06e29c8dfdb
Taylor Holliday <taylor@audulus.com>
2022-02-22 08:51:00 -0800

    Create geom.rs

---
+++ src/geom.rs
@@ -0,0 +1,52 @@
+
+use crate::*;
+
+pub struct Geom<V: View> {
+    child: V,
+    func: Box<dyn Fn(WorldRect)>,
+}
+
+impl<V> View for Geom<V>
+where
+    V: View,
+{
+    fn print(&self, id: ViewID, cx: &mut Context) {
+        println!("Geom {{");
+        (self.child).print(id.child(0), cx);
+        println!("}}");
+    }
+
+    fn process(&self, event: &Event, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.process(event, id.child(0), cx, vger);
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.draw(id.child(0), cx, vger);
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        self.child.layout(id.child(0), sz, cx, vger)
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        self.child.hittest(id.child(0), pt, cx, vger)
+    }
+}
+
+impl<V> Geom<V>
+where
+    V: View + 'static,
+{
+    pub fn new<F: Fn(WorldRect) + 'static>(child: V, f: F) -> Self {
+        Self {
+            child: child,
+            func: Box::new(f)
+        }
+    }
+}
