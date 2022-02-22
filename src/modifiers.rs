4e8d4a226ea6eb881a13c5048e55d4d1b23e995e
Taylor Holliday <taylor@audulus.com>
2022-02-21 22:39:45 -0800

    Update modifiers.rs

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -3,6 +3,7 @@ use crate::*;
 pub trait Modifiers: View + Sized {
     fn padding(self, param: impl Into<PaddingParam>) -> Padding<Self>;
     fn tap<F: Fn() + 'static>(self, f: F) -> Tap<Self>;
+    fn background<BG: View + 'static>(self, background: BG) -> Background<Self, BG>;
 }
 
 impl <V: View + 'static> Modifiers for V {
@@ -11,5 +12,8 @@ impl <V: View + 'static> Modifiers for V {
     }
     fn tap<F: Fn() + 'static>(self, f: F) -> Tap<Self> {
         Tap::new(self, f)
+    }
+    fn background<BG: View + 'static>(self, background: BG) -> Background<Self, BG> {
+        Background::new(self, background)
     }
 }
