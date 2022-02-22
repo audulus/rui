daa2c46a1936fba317cebfe9c7de7a5f9bf480fd
Taylor Holliday <taylor@audulus.com>
2022-02-21 22:49:44 -0800

    Update button.rs

--- src/button.rs
+++ src/button.rs
@@ -103,13 +103,13 @@ pub fn button<F: Fn() + 'static>(name: &str, f: F) -> Button {
     }
 }
 
-pub const BUTTON_PADDING: f32 = 5.0;
+pub const BUTTON_CORNER_RADIUS: f32 = 5.0;
 
 pub fn button2<F: Fn() + 'static>(name: &str, f: F) -> impl View {
     text(&name)
         .padding(Auto)
         .background(
-            rectangle(BUTTON_PADDING)
+            rectangle(BUTTON_CORNER_RADIUS)
                 .color(BUTTON_BACKGROUND_COLOR)
         )
         .tap(f)
