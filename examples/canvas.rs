0ab173f5af0d6a5a69683ed630868d8b804000c4
Taylor Holliday <taylor@audulus.com>
2022-02-23 15:40:18 -0800

    Create canvas.rs

---
+++ examples/canvas.rs
@@ -0,0 +1,22 @@
+use rui::*;
+
+fn main() {
+    rui(hstack! {
+        canvas(|rect, vger| {
+            vger.save();
+            vger.translate(rect.center() - LocalPoint::zero());
+
+            let paint = vger.linear_gradient(
+                [-100.0, -100.0].into(),
+                [100.0, 100.0].into(),
+                AZURE_HIGHLIGHT,
+                RED_HIGHLIGHT,
+                0.0,
+            );
+
+            let radius = 100.0;
+            vger.fill_circle(LocalPoint::zero(), radius, paint);
+            vger.restore();
+        })
+    });
+}
