4d1f55e0f6b58842780ed17051ab2154b60b9556
Taylor Holliday <taylor@audulus.com>
2022-02-23 15:30:29 -0800

    Create canvas.rs

---
+++ src/canvas.rs
@@ -0,0 +1,42 @@
+use crate::*;
+
+pub struct Canvas {
+    func: Box<dyn Fn(LocalRect, &mut VGER)>,
+}
+
+impl View for Canvas {
+    fn print(&self, _id: ViewID, _cx: &mut Context) {
+        println!("canvas");
+    }
+
+    fn process(&self, _event: &Event, _id: ViewID, _cx: &mut Context, vger: &mut VGER) {
+        // do nothing
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        let rect = cx.layout.entry(id).or_insert(LayoutBox::default()).rect;
+
+        (*self.func)(rect, vger)
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        cx.layout.insert(
+            id,
+            LayoutBox {
+                rect: LocalRect::new(LocalPoint::zero(), sz),
+                offset: LocalOffset::zero(),
+            },
+        );
+        sz
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        None
+    }
+}
