d1cbdb27888028bcf97d794029f8e8fe2f4772e6
Taylor Holliday <taylor@audulus.com>
2022-02-22 18:56:03 -0800

    Update gestures.rs

--- src/gestures.rs
+++ src/gestures.rs
@@ -58,3 +58,67 @@ where
         self.child.hittest(id.child(0), pt, cx, vger)
     }
 }
+
+pub struct Drag<V: View> {
+    child: V,
+    func: Box<dyn Fn(LocalOffset)>,
+}
+
+impl<V> Drag<V> where V: View {
+    pub fn new<F: Fn(LocalOffset) + 'static>(v: V, f: F) -> Self {
+        Self { child: v, func: Box::new(f) }
+    }
+}
+
+impl<V> View for Drag<V>
+where
+    V: View,
+{
+    fn print(&self, id: ViewID, cx: &mut Context) {
+        println!("Drag {{");
+        (self.child).print(id.child(0), cx);
+        println!("}}");
+    }
+
+    fn process(&self, event: &Event, vid: ViewID, cx: &mut Context, vger: &mut VGER) {
+        match &event.kind {
+            EventKind::TouchBegin { id } => {
+                if let Some(_) = self.hittest(vid, event.position, cx, vger)
+                {
+                    cx.touches[*id] = vid;
+                    cx.starts[*id] = event.position;
+                }
+            },
+            EventKind::TouchMove { id } => {
+                if cx.touches[*id] == vid {
+                    (*self.func)(event.position - cx.starts[*id]);
+                }
+            },
+            EventKind::TouchEnd { id } => {
+                if cx.touches[*id] == vid {
+                    cx.touches[*id] = ViewID::default();
+                    (*self.func)(event.position - cx.starts[*id]);
+                }
+            }
+            _ => (),
+        }
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.draw(id.child(0), cx, vger)
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        self.child.layout(id.child(0), sz, cx, vger)
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        self.child.hittest(id.child(0), pt, cx, vger)
+    }
+}
