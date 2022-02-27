1c29bafd8be99e3c829bc890bd9c877ee27f7a86
Taylor Holliday <taylor@audulus.com>
2022-02-26 22:39:50 -0800

    Update slider.rs

--- src/slider.rs
+++ src/slider.rs
@@ -2,7 +2,10 @@ use crate::*;
 
 pub fn slider(value: impl Binding<f32> + 'static) -> impl View {
     let x = value.get();
-    circle().offset([x, 0.0].into()).drag(move |off, _state| {
-        value.set(value.get() + off.x);
+    state(0.0, move |width| {
+        let value2 = value.clone();
+        circle().offset([x, 0.0].into()).drag(move |off, _state| {
+            value2.set(value2.get() + off.x);
+        })
     })
 }
