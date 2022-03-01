3edaedf0466cffd844fc9da0707b3b99b3e54ee6
Taylor Holliday <taylor@audulus.com>
2022-03-01 08:43:01 -0800

    Create size.rs

---
+++ src/size.rs
@@ -0,0 +1,59 @@
+use crate::*;
+
+pub struct Size<V: View> {
+    child: V,
+    size: LocalSize,
+}
+
+impl<V> View for Size<V>
+where
+    V: View,
+{
+    fn print(&self, id: ViewID, cx: &mut Context) {
+        println!("Size {{");
+        (self.child).print(id.child(&0), cx);
+        println!("}}");
+    }
+
+    fn process(&self, event: &Event, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.process(event, id.child(&0), cx, vger);
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.draw(id.child(&0), cx, vger);
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        self.child.layout(
+            id.child(&0),
+            self.size,
+            cx,
+            vger,
+        );
+        self.size
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        self.child.hittest(
+            id.child(&0),
+            pt,
+            cx,
+            vger,
+        )
+    }
+}
+
+impl<V> Size<V>
+where
+    V: View + 'static,
+{
+    pub fn new(child: V, size: LocalSize) -> Self {
+        Self { child, size }
+    }
+}
