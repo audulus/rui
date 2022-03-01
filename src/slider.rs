5d2eddb69a1e3ab06d06331d67d66b2390e4663a
Taylor Holliday <taylor@audulus.com>
2022-03-01 08:58:15 -0800

    Update slider.rs

--- src/slider.rs
+++ src/slider.rs
@@ -2,14 +2,18 @@ use crate::*;
 
 pub fn slider(value: impl Binding<f32>) -> impl View {
     let x = value.get();
-    state(0.0, move |width| {
-        let value = value.clone();
-        circle()
-            .size([20.0, 20.0].into())
-            .offset([x, 0.0].into())
-            .drag(move |off, _state| {
-                value.set(value.get() + off.x);
-            })
-            .geom(move |sz| width.set(sz.width))
-    })
+    zstack! {
+        rectangle(2.0).color(BUTTON_BACKGROUND_COLOR);
+        state(0.0, move |width| {
+            let value = value.clone();
+            circle()
+                .size([20.0, 20.0].into())
+                .offset([x, 0.0].into())
+                .drag(move |off, _state| {
+                    value.set(value.get() + off.x);
+                })
+                .geom(move |sz| width.set(sz.width))
+        })
+    }
+    
 }
