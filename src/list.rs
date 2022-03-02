a5b90209f8a1625867ac4ab5342ed73bc2d9d59e
Taylor Holliday <taylor@audulus.com>
2022-03-01 17:33:47 -0800

    Update list.rs

--- src/list.rs
+++ src/list.rs
@@ -99,6 +99,7 @@ where
     }
 }
 
+/// Displays a list of items all of which are represented by the same View. See `examples/list.rs`.
 pub fn list<ID: Hash, V: View, F: Fn(&ID) -> V + 'static>(
     ids: Vec<ID>,
     f: F,
