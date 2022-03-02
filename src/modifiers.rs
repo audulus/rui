8b2ac2adf4e776e632962fd0ff045376adbe9cc8
Taylor Holliday <taylor@audulus.com>
2022-03-01 17:28:37 -0800

    Update modifiers.rs

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -1,12 +1,26 @@
 use crate::*;
 
 pub trait Modifiers: View + Sized {
+
+    /// Adds space around a view. Can be either `Auto` or `Px(number_of_pixels)`
     fn padding(self, param: impl Into<PaddingParam>) -> Padding<Self>;
+
+    /// Calls a function in response to a tap.
     fn tap<F: Fn() + 'static>(self, f: F) -> Tap<Self, F>;
+
+    /// Puts a view behind another. The background view inherits the size of the view.
     fn background<BG: View + 'static>(self, background: BG) -> Background<Self, BG>;
+
+    /// Calls a function with the view's geometry after layout runs.
     fn geom<F: Fn(LocalSize) + 'static>(self, f: F) -> Geom<Self, F>;
+
+    /// Calls a function in response to a drag.
     fn drag<F: Fn(LocalOffset, GestureState) + 'static>(self, f: F) -> Drag<Self, F>;
+
+    /// Applies an offset to the view in local space.
     fn offset(self, offset: LocalOffset) -> Offset<Self>;
+
+    /// Constrains the size of a view.
     fn size(self, size: LocalSize) -> Size<Self>;
 }
 
