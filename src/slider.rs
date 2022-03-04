8215fc77460be779ac1a0315bb8a69b2d712fe26
Taylor Holliday <taylor@audulus.com>
2022-03-03 18:09:11 -0800

    Update slider.rs

--- src/slider.rs
+++ src/slider.rs
@@ -1,5 +1,7 @@
 use crate::*;
 
+const SLIDER_WIDTH :f32 = 4.0;
+
 /// Horizontal slider built from other Views.
 pub fn slider(value: impl Binding<f32>) -> impl View {
     state(0.0, move |width| {
@@ -8,12 +10,21 @@ pub fn slider(value: impl Binding<f32>) -> impl View {
         let value = value.clone();
 
         zstack((
-            rectangle().color(BUTTON_BACKGROUND_COLOR),
+            canvas(|sz, vger| {
+                let c = sz.center();
+                let paint = vger.color_paint(BUTTON_BACKGROUND_COLOR);
+                vger.fill_rect(
+                    [0.0, c.y-SLIDER_WIDTH/2.0].into(),
+                    [sz.width(), c.y+SLIDER_WIDTH/2.0].into(),
+                    0.0,
+                    paint
+                )
+            }),
             circle()
                 .size([20.0, 20.0])
                 .offset([x, 0.0])
                 .drag(move |off, _state| {
-                    value.set(value.get() + off.x / w);
+                    value.set( (value.get() + off.x / w).clamp(0.0,1.0));
                 }),
         ))
         .geom(move |sz| width.set(sz.width))
