6aafc2432ca3137a7fbc0f26acbe506648f3b687
Taylor Holliday <taylor@audulus.com>
2022-03-04 17:34:27 -0800

    Update modifiers.rs

--- src/modifiers.rs
+++ src/modifiers.rs
@@ -47,3 +47,36 @@ impl<V: View + 'static> Modifiers for V {
         Size::new(self, size.into())
     }
 }
+
+#[macro_export]
+macro_rules! modifier_view {
+    () => {
+        fn print(&self, id: ViewID, cx: &mut Context) {
+            self.body.print(id, cx)
+        }
+    
+        fn needs_redraw(&self, id: ViewID, cx: &mut Context) -> bool { self.body.needs_redraw(id, cx) }
+    
+        fn process(&self, event: &Event, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+            self.body.process(event, id, cx, vger)
+        }
+    
+        fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+            self.body.draw(id, cx, vger)
+        }
+    
+        fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+            self.body.layout(id, sz, cx, vger)
+        }
+    
+        fn hittest(
+            &self,
+            id: ViewID,
+            pt: LocalPoint,
+            cx: &mut Context,
+            vger: &mut VGER,
+        ) -> Option<ViewID> {
+            self.body.hittest(id, pt, cx, vger)
+        }
+    }
+}
