e5545d1069297f519d7fafe8c63e2055df850548
Taylor Holliday <taylor@audulus.com>
2022-03-05 15:55:56 -0800

    Create knob.rs

---
+++ src/knob.rs
@@ -0,0 +1,33 @@
+use crate::*;
+
+const THETA_MIN: f32 = 3.0/2.0 * std::f32::consts::PI;
+const THETA_MAX: f32 = 7.0/2.0 * std::f32::consts::PI;
+
+fn lerp(x: f32, a: f32, b: f32) -> f32 {
+    (1.0-x)*a + x*b
+}
+
+pub fn knob(value: impl Binding<f32>) -> impl View {
+
+    let v = value.get();
+    let value = value.clone();
+
+    zstack((
+        circle().color(CLEAR_COLOR).drag(move |off, _state| {
+            value.set((value.get() + (off.x+off.y) / 400.0).clamp(0.0, 1.0));
+        }),
+        canvas(move |sz, vger| {
+            let c = sz.center();
+            let r = sz.width().min(sz.height()) / 2.0;
+            let paint = vger.color_paint(AZURE_HIGHLIGHT);
+            let a0 = lerp(v, THETA_MAX, THETA_MIN);
+            let a1 = THETA_MAX;
+
+            let theta = -(a0 + a1)/2.0 + std::f32::consts::PI;
+            let ap = (a0 - a1).abs()/2.0;
+
+            vger.stroke_arc(c, r, 2.0, theta, ap, paint);
+        }),
+    ))
+
+}
