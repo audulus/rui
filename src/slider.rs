139ed16bb6e061719e7c65d712689fc23ea1822d
Taylor Holliday <taylor@audulus.com>
2022-03-04 20:49:38 -0800

    cleanup

--- src/slider.rs
+++ src/slider.rs
@@ -3,38 +3,6 @@ use crate::*;
 const SLIDER_WIDTH: f32 = 4.0;
 const SLIDER_THUMB_RADIUS: f32 = 10.0;
 
-/// Horizontal slider built from other Views.
-fn hslider_f(value: impl Binding<f32>, thumb_color: Color) -> impl View {
-    state(0.0, move |width| {
-        let w = width.get();
-        let x = value.get() * w;
-        let value = value.clone();
-
-        zstack((
-            rectangle().color(CLEAR_COLOR).drag(move |off, _state| {
-                value.set((value.get() + off.x / w).clamp(0.0, 1.0));
-            }),
-            canvas(move |sz, vger| {
-                let c = sz.center();
-                let paint = vger.color_paint(BUTTON_BACKGROUND_COLOR);
-                vger.fill_rect(
-                    [0.0, c.y - SLIDER_WIDTH / 2.0].into(),
-                    [sz.width(), c.y + SLIDER_WIDTH / 2.0].into(),
-                    0.0,
-                    paint,
-                );
-                let paint = vger.color_paint(thumb_color);
-                vger.fill_circle([x, c.y], SLIDER_THUMB_RADIUS, paint);
-            }),
-        ))
-        .geom(move |sz| {
-            if sz.width != w {
-                width.set(sz.width)
-            }
-        })
-    })
-}
-
 pub struct HSlider<B: Binding<f32>> {
     value: B,
     thumb: Color,
@@ -52,7 +20,36 @@ where
     B: Binding<f32>,
 {
     fn body(&self) -> impl View {
-        hslider_f(self.value.clone(), self.thumb)
+        let value = self.value.clone();
+        let thumb_color = self.thumb;
+        state(0.0, move |width| {
+            let w = width.get();
+            let x = value.get() * w;
+            let value = value.clone();
+    
+            zstack((
+                rectangle().color(CLEAR_COLOR).drag(move |off, _state| {
+                    value.set((value.get() + off.x / w).clamp(0.0, 1.0));
+                }),
+                canvas(move |sz, vger| {
+                    let c = sz.center();
+                    let paint = vger.color_paint(BUTTON_BACKGROUND_COLOR);
+                    vger.fill_rect(
+                        [0.0, c.y - SLIDER_WIDTH / 2.0].into(),
+                        [sz.width(), c.y + SLIDER_WIDTH / 2.0].into(),
+                        0.0,
+                        paint,
+                    );
+                    let paint = vger.color_paint(thumb_color);
+                    vger.fill_circle([x, c.y], SLIDER_THUMB_RADIUS, paint);
+                }),
+            ))
+            .geom(move |sz| {
+                if sz.width != w {
+                    width.set(sz.width)
+                }
+            })
+        })
     }
 
     pub fn thumb_color(self, thumb_color: Color) -> Self {
