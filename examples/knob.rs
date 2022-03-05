fcbab34d911ab60520321a23521c143019dc5643
Taylor Holliday <taylor@audulus.com>
2022-03-05 15:59:25 -0800

    Create knob.rs

---
+++ examples/knob.rs
@@ -0,0 +1,16 @@
+use rui::*;
+
+#[derive(Clone)]
+struct MyState {
+    value: f32,
+}
+
+fn main() {
+    rui(state(MyState { value: 0.0 }, |state| {
+        vstack((
+            text(&format!("value: {:?}", state.get().value)).padding(Auto),
+            knob(bind!(state, value))
+                .padding(Auto),
+        ))
+    }));
+}
