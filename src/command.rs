18e2794971941e2ff037f2a1c6cd8b641628fe0d
Taylor Holliday <taylor@audulus.com>
2022-03-09 07:52:43 -0800

    add CommandGroup

--- src/command.rs
+++ src/command.rs
@@ -67,15 +67,17 @@ where
 
 pub trait CommandBase {
     fn exec(&self);
+    fn name(&self) -> String;
+    fn key(&self) -> Option<KeyCode>;
 }
 
 pub trait CommandTuple {
-    fn foreach_view<F: FnMut(&dyn CommandBase)>(&self, f: &mut F);
+    fn foreach_cmd<F: FnMut(&dyn CommandBase)>(&self, f: &mut F);
     fn len(&self) -> usize;
 }
 
 impl<A: CommandBase> CommandTuple for (A,) {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
     }
     fn len(&self) -> usize {
@@ -84,7 +86,7 @@ impl<A: CommandBase> CommandTuple for (A,) {
 }
 
 impl<A: CommandBase, B: CommandBase> CommandTuple for (A, B) {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
     }
@@ -94,7 +96,7 @@ impl<A: CommandBase, B: CommandBase> CommandTuple for (A, B) {
 }
 
 impl<A: CommandBase, B: CommandBase, C: CommandBase> CommandTuple for (A, B, C) {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
         f(&self.2);
@@ -105,7 +107,7 @@ impl<A: CommandBase, B: CommandBase, C: CommandBase> CommandTuple for (A, B, C) 
 }
 
 impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase> CommandTuple for (A, B, C, D) {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
         f(&self.2);
@@ -117,7 +119,7 @@ impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase> CommandTupl
 }
 
 impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandBase> CommandTuple for (A, B, C, D, E) {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
         f(&self.2);
@@ -130,7 +132,7 @@ impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandB
 }
 
 impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandBase, F: CommandBase> CommandTuple for (A, B, C, D, E, F) {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
         f(&self.2);
@@ -146,7 +148,7 @@ impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandB
 impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandBase, F: CommandBase, G: CommandBase> CommandTuple
     for (A, B, C, D, E, F, G)
 {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
         f(&self.2);
@@ -163,7 +165,7 @@ impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandB
 impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandBase, F: CommandBase, G: CommandBase, H: CommandBase> CommandTuple
     for (A, B, C, D, E, F, G, H)
 {
-    fn foreach_view<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
+    fn foreach_cmd<FN: FnMut(&dyn CommandBase)>(&self, f: &mut FN) {
         f(&self.0);
         f(&self.1);
         f(&self.2);
@@ -175,5 +177,72 @@ impl<A: CommandBase, B: CommandBase, C: CommandBase, D: CommandBase, E: CommandB
     }
     fn len(&self) -> usize {
         8
+    }
+}
+
+pub struct CommandGroup<V: View, C: CommandTuple> {
+    child: V,
+    cmds: C
+}
+
+impl<V, C> CommandGroup<V, C>
+where
+    V: View,
+    C: CommandTuple,
+{
+    pub fn new(v: V, cmds: C) -> Self {
+        Self { child: v, cmds }
+    }
+}
+
+impl<V, C> View for CommandGroup<V, C>
+where
+    V: View,
+    C: CommandTuple,
+{
+    fn print(&self, id: ViewID, cx: &mut Context) {
+        println!("Command {{");
+        (self.child).print(id.child(&0), cx);
+        println!("}}");
+    }
+
+    fn needs_redraw(&self, id: ViewID, cx: &mut Context) -> bool {
+        self.child.needs_redraw(id.child(&0), cx)
+    }
+
+    fn process(&self, event: &Event, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        if let EventKind::Command(name) = &event.kind {
+            self.cmds.foreach_cmd(&mut |cmd| {
+                if cmd.name() == *name {
+                    cmd.exec();
+                }
+            });
+        }
+        self.child.process(event, id.child(&0), cx, vger)
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.draw(id.child(&0), cx, vger)
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        self.child.layout(id.child(&0), sz, cx, vger)
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        self.child.hittest(id.child(&0), pt, cx, vger)
+    }
+
+    fn commands(&self, id: ViewID, cx: &mut Context, cmds: &mut Vec<CommandInfo>) {
+        self.child.commands(id.child(&0), cx, cmds);
+        self.cmds.foreach_cmd(&mut |cmd| {
+            cmds.push(CommandInfo{ path: cmd.name().clone(), key: cmd.key() } )
+        });
     }
 }
