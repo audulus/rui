21a2935fc48c8aa368b83fbf9998d1aab59f0e68
Taylor Holliday <taylor@audulus.com>
2022-03-13 14:17:23 -0700

    Save off the glyph rects w/o causing a redraw.

--- src/text_editor.rs
+++ src/text_editor.rs
@@ -1,8 +1,22 @@
 pub use crate::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+
+struct TextEditorGlyphInfo {
+    glyph_rects: Vec<LocalRect>
+}
+
+impl TextEditorGlyphInfo {
+    fn new() -> Self {
+        Self { glyph_rects: vec![] }
+    }
+}
 
 #[derive(Clone)]
 struct TextEditorState {
-    cursor: usize
+    cursor: usize,
+    glyph_info: Rc<RefCell<TextEditorGlyphInfo>>
 }
 
 impl TextEditorState {
@@ -19,12 +33,22 @@ impl TextEditorState {
     }
 }
 
+impl TextEditorState {
+    fn new() -> Self {
+        Self {
+            cursor: 0,
+            glyph_info: Rc::new(RefCell::new(TextEditorGlyphInfo::new()))
+        }
+    }
+}
+
 pub fn text_editor(text: impl Binding<String>) -> impl View {
     let len = text.get().len();
-    state( TextEditorState{ cursor: 0 }, move |state| {
+    state( TextEditorState::new(), move |state| {
         let text = text.clone();
         let text2 = text.clone();
         let cursor = state.with(|s| s.cursor);
+        let state2 = state.clone();
         canvas(move |rect, vger| {
             vger.translate([0.0, rect.height()]);
             let font_size = 18;
@@ -33,6 +57,8 @@ pub fn text_editor(text: impl Binding<String>) -> impl View {
             let rects = vger.glyph_positions(&text.get(), font_size, break_width);
             let glyph_rect_paint = vger.color_paint(vger::Color::MAGENTA);
             vger.fill_rect(rects[cursor], 0.0, glyph_rect_paint);
+
+            state2.get().glyph_info.borrow_mut().glyph_rects = rects;
 
             vger.text(&text.get(), font_size, TEXT_COLOR, break_width);
             
