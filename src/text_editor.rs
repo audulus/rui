7a9b4fe6bf3c4229c67b920dc93eab27307ba4ea
Taylor Holliday <taylor@audulus.com>
2022-03-13 10:16:25 -0700

    Update text_editor.rs

--- src/text_editor.rs
+++ src/text_editor.rs
@@ -5,7 +5,22 @@ struct TextEditorState {
     cursor: usize
 }
 
+impl TextEditorState {
+    fn fwd(&mut self, len: usize) {
+        self.cursor += 1;
+        if self.cursor >= len {
+            self.cursor = len-1;
+        }
+    }
+    fn back(&mut self) {
+        if self.cursor > 0 {
+            self.cursor -= 1;
+        }
+    }
+}
+
 pub fn text_editor(text: impl Binding<String>) -> impl View {
+    let len = text.get().len();
     state( TextEditorState{ cursor: 0 }, move |state| {
         let text = text.clone();
         canvas(move |rect, vger| {
@@ -15,8 +30,8 @@ pub fn text_editor(text: impl Binding<String>) -> impl View {
             vger.text(&text.get(), font_size, TEXT_COLOR, break_width);
         }).key(move |k| {
             match k {
-                KeyCode::ArrowLeft => state.with_mut(|s| s.cursor -= 1),
-                KeyCode::ArrowRight => state.with_mut(|s| s.cursor += 1),
+                KeyCode::ArrowLeft => state.with_mut(|s| s.fwd(len) ),
+                KeyCode::ArrowRight => state.with_mut(|s| s.back() ),
                 _ => ()
             }
         })
