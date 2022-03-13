f66cc349e4a1b9b20be600d7c66a8d69a3c79ba3
Taylor Holliday <taylor@audulus.com>
2022-03-13 10:22:40 -0700

    Update text_editor.rs

--- src/text_editor.rs
+++ src/text_editor.rs
@@ -23,11 +23,16 @@ pub fn text_editor(text: impl Binding<String>) -> impl View {
     let len = text.get().len();
     state( TextEditorState{ cursor: 0 }, move |state| {
         let text = text.clone();
+        let cursor = state.with(|s| s.cursor);
         canvas(move |rect, vger| {
             vger.translate([0.0, rect.height()]);
             let font_size = 18;
             let break_width = Some(rect.width());
             vger.text(&text.get(), font_size, TEXT_COLOR, break_width);
+
+            let rects = vger.glyph_positions(&text.get(), font_size, break_width);
+            let glyph_rect_paint = vger.color_paint(vger::Color::MAGENTA.alpha(0.1));
+            vger.fill_rect(rects[cursor], 0.0, glyph_rect_paint);
         }).key(move |k| {
             match k {
                 KeyCode::ArrowLeft => state.with_mut(|s| s.fwd(len) ),
