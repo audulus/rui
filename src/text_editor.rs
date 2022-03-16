8f7ba29d4d05e34911a939308e419f9a27e57816
Taylor Holliday <taylor@audulus.com>
2022-03-15 20:03:05 -0700

    Update text_editor.rs

--- src/text_editor.rs
+++ src/text_editor.rs
@@ -163,45 +163,7 @@ where
 }
 
 impl<B> View for TextEditor<B> where B: Binding<String> {
-    fn print(&self, _id: ViewID, _cx: &mut Context) {
-        println!("text_editor");
-    }
-
-    fn needs_redraw(&self, id: ViewID, cx: &mut Context) -> bool {
-        self.body().needs_redraw(id, cx)
-    }
-
-    fn process(&self, event: &Event, id: ViewID, cx: &mut Context, vger: &mut VGER) {
-        self.body().process(event, id, cx, vger)
-    }
-
-    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
-        self.body().draw(id, cx, vger)
-    }
-
-    fn layout(
-        &self,
-        id: ViewID,
-        sz: LocalSize,
-        cx: &mut Context,
-        vger: &mut VGER,
-    ) -> LocalSize {
-        self.body().layout(id, sz, cx, vger)
-    }
-
-    fn hittest(
-        &self,
-        id: ViewID,
-        pt: LocalPoint,
-        cx: &mut Context,
-        vger: &mut VGER,
-    ) -> Option<ViewID> {
-        self.body().hittest(id, pt, cx, vger)
-    }
-
-    fn commands(&self, id: ViewID, cx: &mut Context, cmds: &mut Vec<CommandInfo>) {
-        self.body().commands(id, cx, cmds);
-    }
+    body_view!();
 }
 
 pub fn text_editor(text: impl Binding<String>) -> impl View {
