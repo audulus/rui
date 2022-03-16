29f9439e7883e5ee1358ef2fb57e4c6a960af4c2
Taylor Holliday <taylor@audulus.com>
2022-03-16 08:58:15 -0700

    Update text_editor.rs

--- examples/text_editor.rs
+++ examples/text_editor.rs
@@ -2,5 +2,12 @@ use rui::*;
 
 fn main() {
     let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    rui(state(lorem.to_string(), |state| text_editor(state).padding(Auto)));
+    rui(vstack((
+        state(lorem.to_string(), |state| text_editor(state).padding(Auto))
+            .background(rectangle().color(BUTTON_BACKGROUND_COLOR))
+            .padding(Auto),
+        state(lorem.to_string(), |state| text_editor(state).padding(Auto))
+            .background(rectangle().color(BUTTON_BACKGROUND_COLOR))
+            .padding(Auto)
+    )));
 }
