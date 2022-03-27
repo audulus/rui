395285953d2db0534f8b32b09c6e13978182ce0a
Taylor Holliday <taylor@audulus.com>
2022-03-26 17:43:20 -0700

    Update toggle.rs

--- src/toggle.rs
+++ src/toggle.rs
@@ -4,12 +4,12 @@ pub fn toggle(state: impl Binding<bool>) -> impl View {
     let b = state.get();
     zstack((
         rectangle()
-            .color(BUTTON_BACKGROUND_COLOR)
+            .color(if b { AZURE_HIGHLIGHT_BACKGROUND } else { CONTROL_BACKGROUND })
             .corner_radius(10.0)
             .size([40.0, 20.0])
             .tap(move || { state.with_mut(|b| *b = !*b) }),
         circle()
-            .color(RED_HIGHLIGHT)
+            .color(if b { AZURE_HIGHLIGHT } else { MEDIUM_GRAY })
             .size([10.0, 10.0])
             .offset([if b { 25.0 } else { 5.0 }, 5.0])
     ))
