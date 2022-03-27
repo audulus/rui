5480e4bf1f5c71f4339a52227cc47deaadca2fd1
Taylor Holliday <taylor@audulus.com>
2022-03-27 11:48:22 -0700

    Update gallery.rs

--- examples/gallery.rs
+++ examples/gallery.rs
@@ -1,21 +1,33 @@
 use rui::*;
 
+fn slider_example() -> impl View {
+    hstack((
+        text("slider").padding(Auto),
+        state(0.5, |s| hslider(s))
+    ))
+}
+
+fn knob_example() -> impl View {
+    hstack((
+        text("knob").padding(Auto),
+        state(0.5, |s| knob(s).size([30.0, 30.0]).padding(Auto))
+    ))
+}
+
+fn toggle_example() -> impl View {
+    hstack((
+        text("toggle").padding(Auto),
+        state(false, |b| toggle(b).size([30.0, 30.0]).padding(Auto))
+    ))
+}
+
 fn main() {
 
     rui(vstack((
         text("rui widget gallery"),
-        hstack((
-            text("slider").padding(Auto),
-            state(0.5, |s| hslider(s))
-        )),
-        hstack((
-            text("knob").padding(Auto),
-            state(0.5, |s| knob(s).size([30.0, 30.0]).padding(Auto))
-        )),
-        hstack((
-            text("toggle").padding(Auto),
-            state(false, |b| toggle(b).size([30.0, 30.0]).padding(Auto))
-        )),
+        slider_example(),
+        knob_example(),
+        toggle_example()
     )).padding(Auto))
 
 }
