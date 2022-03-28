e3bfc22e445d6cf2751553680bfc70005dcae900
Taylor Holliday <taylor@audulus.com>
2022-03-28 16:32:45 -0700

    Update async.rs

--- examples/async.rs
+++ examples/async.rs
@@ -1,4 +1,5 @@
 use rui::*;
+use std::thread;
 
 fn main() {
     rui(
@@ -8,6 +9,11 @@ fn main() {
                 button(text("press to begin"), move || {
                     println!("beginning task");
                     s.with_mut(|s| *s = "task started".to_string());
+
+                    thread::spawn(|| {
+                        // thread code
+                        println!("inside task");
+                    });
                 }),
                 text(&txt)
             ))
