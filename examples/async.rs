b4a5e4eba653522dd06c7c70e374be77cfc65a65
Taylor Holliday <taylor@audulus.com>
2022-03-28 16:39:09 -0700

    Update async.rs

--- examples/async.rs
+++ examples/async.rs
@@ -7,14 +7,11 @@ fn main() {
             let txt = s.get();
             hstack((
                 button(text("press to begin"), move || {
-                    println!("beginning task");
                     s.with_mut(|s| *s = "task started".to_string());
                     let s2 = s.clone();
 
                     thread::spawn(move || {
-                        println!("inside task");
                         thread::sleep(time::Duration::from_secs(2));
-                        println!("task finished");
                         s2.with_mut(|s| *s = "task complete".to_string());
                     });
                 }),
