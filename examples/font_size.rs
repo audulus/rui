6f31a61d7d7e9eb8c7c9c5dc4a8c357d7aa5a0f5
Taylor Holliday <taylor@audulus.com>
2022-04-04 09:44:50 -0700

    Create font_size.rs

---
+++ examples/font_size.rs
@@ -0,0 +1,12 @@
+use rui::*;
+
+fn main() {
+    rui(state(0.0, |size| {
+        let s = (size.get() * 100.0) as u32;
+        vstack((
+            text("58").font_size(s),
+            text(&format!("font size: {}", s)),
+            hslider(size.clone())
+        ))
+    }));
+}
