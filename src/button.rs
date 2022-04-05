e0354ef77c13d2e81ec6a7462b89402a053d990b
Taylor Holliday <taylor@audulus.com>
2022-04-04 17:48:52 -0700

    Update button.rs

--- src/button.rs
+++ src/button.rs
@@ -1,4 +1,5 @@
 pub use crate::*;
+use accesskit::Role;
 
 pub const BUTTON_CORNER_RADIUS: f32 = 5.0;
 
@@ -11,4 +12,5 @@ pub fn button<F: Fn() + 'static>(view: impl View + 'static, f: F) -> impl View {
                 .color(BUTTON_BACKGROUND_COLOR),
         )
         .tap(f)
+        .role(Role::Button)
 }
