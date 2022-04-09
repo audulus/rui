733f0a9e4f7ef7917654a14479bb2c41cd21c608
Taylor Holliday <taylor@audulus.com>
2022-04-08 18:52:29 -0700

    Update gestures.rs

--- src/gestures.rs
+++ src/gestures.rs
@@ -181,3 +181,88 @@ where
 }
 
 impl<V, F> private::Sealed for Drag<V, F> {}
+
+
+
+/// Struct for the `tap` gesture.
+pub struct Tap2<V, F> {
+    child: V,
+    func: F,
+}
+
+impl<'a, V, F> Tap2<V, F>
+where
+    V: View,
+    F: Fn() + 'a,
+{
+    pub fn new(v: V, f: F) -> Self {
+        Self { child: v, func: f }
+    }
+}
+
+impl<'a, V, F> View for Tap2<V, F>
+where
+    V: View,
+    F: Fn() + 'a,
+{
+    fn print(&self, id: ViewID, cx: &mut Context) {
+        println!("Tap {{");
+        (self.child).print(id.child(&0), cx);
+        println!("}}");
+    }
+
+    fn process(&self, event: &Event, vid: ViewID, cx: &mut Context, vger: &mut VGER) {
+        match &event.kind {
+            EventKind::TouchBegin { id } => {
+                if let Some(_) = self.hittest(vid, event.position, cx, vger) {
+                    cx.touches[*id] = vid;
+                }
+            }
+            EventKind::TouchEnd { id } => {
+                if cx.touches[*id] == vid {
+                    cx.touches[*id] = ViewID::default();
+                    let f = &self.func;
+                    f();
+                }
+            }
+            _ => (),
+        }
+    }
+
+    fn draw(&self, id: ViewID, cx: &mut Context, vger: &mut VGER) {
+        self.child.draw(id.child(&0), cx, vger)
+    }
+
+    fn layout(&self, id: ViewID, sz: LocalSize, cx: &mut Context, vger: &mut VGER) -> LocalSize {
+        self.child.layout(id.child(&0), sz, cx, vger)
+    }
+
+    fn hittest(
+        &self,
+        id: ViewID,
+        pt: LocalPoint,
+        cx: &mut Context,
+        vger: &mut VGER,
+    ) -> Option<ViewID> {
+        self.child.hittest(id.child(&0), pt, cx, vger)
+    }
+
+    fn commands(&self, id: ViewID, cx: &mut Context, cmds: &mut Vec<CommandInfo>) {
+        self.child.commands(id.child(&0), cx, cmds)
+    }
+
+    fn gc(&self, id: ViewID, cx: &mut Context, map: &mut StateMap) {
+        self.child.gc(id.child(&0), cx, map)
+    }
+
+    fn access(
+        &self,
+        id: ViewID,
+        cx: &mut Context,
+        nodes: &mut Vec<accesskit::Node>,
+    ) -> Option<accesskit::NodeId> {
+        self.child.access(id.child(&0), cx, nodes)
+    }
+}
+
+impl<V, F> private::Sealed for Tap2<V, F> {}
