abc951c430175f594b8bececff092304e2624ed6
Taylor Holliday <taylor@audulus.com>
2022-04-09 00:09:50 -0700

    Update counter2.rs

--- examples/counter2.rs
+++ examples/counter2.rs
@@ -1,15 +1,16 @@
 use rui::*;
 
 fn main() {
-    /*
-    let s = state2(
+    rui(gstate(
         || 1,
         |count| {
-            circle().tap2(|| *count += 1)
+            vstack((
+                text(&format!("{}", count.get())).padding(Auto),
+                button(text("increment"), move || {
+                    count.with_mut(|x| *x += 1);
+                })
+                .padding(Auto),
+            ))
         },
-    );*/
-
-    let mut count = 0;
-    let countref = &mut count;
-    circle().tap2(|| *countref += 1);
+    ));
 }
