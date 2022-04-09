770584fe518970a52a581d3913f55066bb3d191a
Taylor Holliday <taylor@audulus.com>
2022-04-09 10:34:09 -0700

    add thread local variable to disable state dirtying

--- src/state.rs
+++ src/state.rs
@@ -2,16 +2,25 @@ use std::any::Any;
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
 use tao::event_loop::EventLoopProxy;
+use std::cell::RefCell;
 use crate::*;
 
 static STATE_DIRTY: AtomicBool = AtomicBool::new(false);
+
+thread_local! {
+    pub static ENABLE_DIRTY: RefCell<bool> = RefCell::new(true);
+}
 
 pub(crate) fn is_state_dirty() -> bool {
     STATE_DIRTY.load(Ordering::Relaxed)
 }
 
 pub(crate) fn set_state_dirty() {
-    STATE_DIRTY.store(true, Ordering::Relaxed);
+    ENABLE_DIRTY.with(|enable| {
+        if *enable.borrow() {
+            STATE_DIRTY.store(true, Ordering::Relaxed);
+        }
+    })
 }
 
 pub(crate) fn clear_state_dirty() {
