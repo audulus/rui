2cdffc9d069bf8736b5b5f926d3b7db588213b5c
Taylor Holliday <taylor@audulus.com>
2022-04-09 17:11:32 -0700

    Update binding.rs

--- src/binding.rs
+++ src/binding.rs
@@ -207,3 +207,21 @@ mod tests {
         assert_eq!(s.get().x, 42);
     }
 }
+
+
+/// Reads or writes a value owned by a source-of-truth.
+pub trait GBinding<S>: Clone + Copy + 'static {
+    fn with<T, F: FnOnce(&S) -> T>(&self, f: F) -> T;
+    fn with_mut<T, F: FnOnce(&mut S) -> T>(&self, f: F) -> T;
+
+    fn get(&self) -> S
+    where
+        S: Clone,
+    {
+        self.with(|s| s.clone())
+    }
+
+    fn set(&self, value: S) {
+        self.with_mut(move |s| *s = value);
+    }
+}
