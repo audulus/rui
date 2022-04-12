00f0045df0e593527be6041ae37442d41d536ba6
Taylor Holliday <taylor@audulus.com>
2022-04-11 21:38:48 -0700

    Update async.rs

--- examples/async.rs
+++ examples/async.rs
@@ -8,13 +8,13 @@ fn main() {
     rui(state(
         || "task not started".to_string(),
         |s, cx| {
-            let txt = s.get();
+            let txt = &cx[s];
             hstack((
                 button(text("press to begin"), move |_cx| {
                     spawn(move || {
-                        on_main(move |_cx| s.set("task started".into()));
+                        on_main(move |cx| cx[s] = "task started".into() );
                         sleep(Duration::from_secs(2));
-                        on_main(move |_cx| s.set("task complete".into()));
+                        on_main(move |cx| cx[s] = "task complete".into() );
                     });
                 }),
                 text(&txt),
