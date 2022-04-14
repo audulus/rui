96085fd9ad28deacc14e41c72da63fd00ce27a0d
Taylor Holliday <taylor@audulus.com>
2022-04-14 10:28:52 -0700

    Update knob.rs

--- src/knob.rs
+++ src/knob.rs
@@ -9,28 +9,7 @@ fn lerp(x: f32, a: f32, b: f32) -> f32 {
 
 /// Knob for controlling a 0 to 1 floating point parameter.
 pub fn knob(value: impl Binding<f32>) -> impl View {
-    zstack((
-        circle().color(CLEAR_COLOR).drag(move |cx, off, _state| {
-            value.with_mut(cx, |v| *v = (*v + (off.x + off.y) / 400.0).clamp(0.0, 1.0));
-        }),
-        canvas(move |cx, sz, vger| {
-            let c = sz.center();
-            let r = sz.width().min(sz.height()) / 2.0;
-
-            let paint = vger.color_paint(CONTROL_BACKGROUND);
-
-            vger.stroke_arc(c, r, 2.0, 0.0, std::f32::consts::PI, paint);
-
-            let paint = vger.color_paint(AZURE_HIGHLIGHT);
-            let a0 = lerp(*value.get(cx), THETA_MAX, THETA_MIN);
-            let a1 = THETA_MAX;
-
-            let theta = -(a0 + a1) / 2.0 + std::f32::consts::PI;
-            let ap = (a0 - a1).abs() / 2.0;
-
-            vger.stroke_arc(c, r, 2.0, theta, ap, paint);
-        }),
-    ))
+    state(||(), move |_, cx| knob_v(*value.get(cx), setter(value)))
 }
 
 /// Knob for controlling a 0 to 1 floating point parameter.
