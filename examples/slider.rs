f3348a320a869d69fa29844d8379046bbca8cfde
Taylor Holliday <taylor@audulus.com>
2022-04-14 14:11:26 -0700

    implement View for f32

--- examples/slider.rs
+++ examples/slider.rs
@@ -10,7 +10,7 @@ make_lens!(ValueLens, MyState, f32, value);
 fn main() {
     rui(state(MyState::default, |state, cx| {
         vstack((
-            format!("value: {:?}", cx[state].value)
+            cx[state].value
                 .font_size(10)
                 .padding(Auto),
             hslider(bind(state, ValueLens {}))
