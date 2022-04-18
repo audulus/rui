8372e4f7f5ef990147e200d34e4aa066f6189d30
Taylor Holliday <taylor@audulus.com>
2022-04-18 15:01:51 -0700

    add animation

--- examples/gestures.rs
+++ examples/gestures.rs
@@ -6,16 +6,25 @@ fn main() {
             .color(RED_HIGHLIGHT.alpha(0.8))
             .tap(|_cx, _key_mods| println!("tapped circle"))
             .padding(Auto),
-        state(LocalOffset::zero, |off, cx| {
-            rectangle()
+        state(LocalOffset::zero, move |off, _| { // target offset
+            state(LocalOffset::zero, move |anim_off, cx| { // animated offset
+                rectangle()
                 .corner_radius(5.0)
                 .color(AZURE_HIGHLIGHT.alpha(0.8))
-                .offset(cx[off])
-                .drag(move |cx, delta, _state, _key_mods, mouse_button| {
+                .offset(cx[anim_off])
+                .drag(move |cx, delta, state, _key_mods, mouse_button| {
                     println!("mouse button {:?}", mouse_button);
-                    cx[off] += delta
+                    cx[off] += delta;
+                    cx[anim_off] = cx[off];
+                    if state == GestureState::Ended {
+                        cx[off] = LocalOffset::zero();
+                    }
+                })
+                .anim(move |cx, dt| {
+                    cx[anim_off] = cx[anim_off].lerp(cx[off], 0.05);
                 })
                 .padding(Auto)
+            })
         }),
     )));
 }
