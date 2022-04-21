f3f489e270d0fc5c78d0672056435d9494b860ef
Taylor Holliday <taylor@audulus.com>
2022-04-21 12:27:26 -0700

    Create lens.rs

---
+++ examples/lens.rs
@@ -0,0 +1,19 @@
+use rui::*;
+
+#[derive(Default)]
+struct MyState {
+    value: f32,
+}
+
+make_lens!(ValueLens, MyState, f32, value);
+
+fn main() {
+    rui(state(MyState::default, |state, cx| {
+        vstack((
+            cx[state].value.font_size(10).padding(Auto),
+            hslider(bind(state, ValueLens {}))
+                .thumb_color(RED_HIGHLIGHT)
+                .padding(Auto),
+        ))
+    }));
+}
