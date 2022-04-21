a46f399761304f67b00a1c373d9323e8d7feef92
Taylor Holliday <taylor@audulus.com>
2022-04-21 10:41:33 -0700

    Update event.rs

--- src/event.rs
+++ src/event.rs
@@ -3,11 +3,23 @@ use crate::*;
 /// Type of event.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum EventKind {
+
+    /// Touch event, or mouse down.
     TouchBegin { id: usize },
+
+    /// Touch moved or mouse moved while down.
     TouchMove { id: usize },
+
+    /// Touch went up or mouse button released.
     TouchEnd { id: usize },
+
+    /// Menu command.
     Command(String),
+
+    /// Key press.
     Key(KeyPress),
+
+    /// Animation.
     Anim,
 }
 
