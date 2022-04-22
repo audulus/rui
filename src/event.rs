720f27bb55aa403530f02a8921d18a01765f4edd
Taylor Holliday <taylor@audulus.com>
2022-04-22 16:37:33 -0700

    Update event.rs

--- src/event.rs
+++ src/event.rs
@@ -93,3 +93,33 @@ pub enum Key {
     F11,
     F12,
 }
+
+#[derive(Clone, Debug)]
+pub enum HotKey {
+    KeyA,
+    KeyB,
+    KeyC,
+    KeyD,
+    KeyE,
+    KeyF,
+    KeyG,
+    KeyH,
+    KeyI,
+    KeyJ,
+    KeyK,
+    KeyL,
+    KeyM,
+    KeyN,
+    KeyO,
+    KeyP,
+    KeyQ,
+    KeyR,
+    KeyS,
+    KeyT,
+    KeyU,
+    KeyV,
+    KeyW,
+    KeyX,
+    KeyY,
+    KeyZ,
+}
