0887075566f9cb2b97e52438a0a855bcd9d33651
Taylor Holliday <taylor@audulus.com>
2022-06-26 12:21:00 -0700

    improve list example

--- examples/list.rs
+++ examples/list.rs
@@ -1,34 +1,9 @@
 use rui::*;
 
-struct MyData {
-    id: i32,
-    name: String,
-}
-
 fn main() {
-    let data = vec![
-        MyData {
-            id: 0,
-            name: "John".into(),
-        },
-        MyData {
-            id: 1,
-            name: "Paul".into(),
-        },
-        MyData {
-            id: 2,
-            name: "George".into(),
-        },
-        MyData {
-            id: 3,
-            name: "Ringo".into(),
-        },
-    ];
+    let data = vec!["John", "Paul", "George", "Ringo"];
 
-    rui(list(data.iter().map(|d| d.id).collect(), move |id| {
-        // Linear search is not good. Use a better data structure for production code.
-        let d = data.iter().find(|d| d.id == *id).unwrap();
-
-        hstack((circle(), format!("{}", d.name)))
+    rui(list((0usize..data.len()).collect(), move |id| {
+        hstack((circle(), format!("{}", &data[*id])))
     }));
 }
