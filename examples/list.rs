11d0b847990ffe5e1110b2994480a6eeff2a4fc7
Taylor Holliday <taylor@audulus.com>
2022-06-26 12:23:32 -0700

    cleanup

--- examples/list.rs
+++ examples/list.rs
@@ -3,7 +3,9 @@ use rui::*;
 fn main() {
     let data = vec!["John", "Paul", "George", "Ringo"];
 
-    rui(list((0usize..data.len()).collect(), move |id| {
+    let ids = (0usize..data.len()).collect();
+
+    rui(list(ids, move |id| {
         hstack((circle(), format!("{}", &data[*id])))
     }));
 }
