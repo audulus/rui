18e2f0b318fdc7492ebe2140e30e3c887e7744a1
Taylor Holliday <wtholliday@gmail.com>
2022-10-08 15:20:49 -0700

    add test

--- src/views/button.rs
+++ src/views/button.rs
@@ -14,3 +14,52 @@ pub fn button<A: 'static, F: Fn(&mut Context) -> A + 'static>(view: impl View, f
         .tap(move |cx| f(cx))
         .role(Role::Button)
 }
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_button() {
+        let mut cx = Context::new();
+
+        let ui = state(|| false, |pushed, _| button("button", move |cx| { *pushed.get_mut(cx) = true; }));
+        let sz = [100.0, 100.0].into();
+
+        let button_sz = ui.layout(
+            cx.root_id,
+            &mut LayoutArgs {
+                sz,
+                cx: &mut cx,
+                text_bounds: &mut |_, _, _| LocalRect::new(LocalPoint::zero(), [90.0, 90.0].into())
+            },
+        );
+
+        assert_eq!(button_sz, sz);
+        let s = State::<bool>::new(cx.root_id);
+        assert_eq!(*s.get(&mut cx), false);
+
+        let events = [
+            Event::TouchBegin {
+                id: 0,
+                position: [50.0, 50.0].into(),
+            },
+            Event::TouchEnd {
+                id: 0,
+                position: [50.0, 50.0].into(),
+            },
+        ];
+
+        let mut actions = vec![];
+        for event in &events {
+            ui.process(event, cx.root_id, &mut cx, &mut actions);
+        }
+
+        assert!(cx.state_map.contains_key(&cx.root_id));
+
+        // State should have changed.
+        assert_eq!(*s.get(&mut cx), true);
+    }
+}
