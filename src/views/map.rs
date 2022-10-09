b18bb513615485493968ac10d769c3d06531a483
Taylor Holliday <taylor@audulus.com>
2022-10-09 09:56:43 -0700

    Cleanup

--- src/views/map.rs
+++ src/views/map.rs
@@ -39,17 +39,7 @@ where
     fn layout(&self, id: ViewId, args: &mut LayoutArgs) -> LocalSize {
         args.cx.set_state(id, self.value.clone());
 
-        let child_size = (self.func)(State::new(id), args.cx).layout(id.child(&0), args);
-
-        args.cx.layout.insert(
-            id,
-            LayoutBox {
-                rect: LocalRect::new(LocalPoint::zero(), child_size),
-                offset: LocalOffset::zero(),
-            },
-        );
-
-        child_size
+        (self.func)(State::new(id), args.cx).layout(id.child(&0), args)
     }
 
     fn dirty(&self, id: ViewId, xform: LocalToWorld, cx: &mut Context) {
