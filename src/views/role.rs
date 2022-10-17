c883795fbe7404e4422d8b0fdf75a534876070a4
Taylor Holliday <wtholliday@gmail.com>
2022-10-17 09:25:42 -0700

    Update role.rs

--- src/views/role.rs
+++ src/views/role.rs
@@ -43,6 +43,10 @@ where
         self.child.dirty(id.child(&0), xform, cx);
     }
 
+    fn bounds(&self, id: ViewId, xform: LocalToWorld) -> LocalRect {
+        self.child.bounds(id.child(&0), xform)
+    }
+
     fn hittest(&self, id: ViewId, pt: LocalPoint, cx: &mut Context) -> Option<ViewId> {
         self.child.hittest(id.child(&0), pt, cx)
     }
