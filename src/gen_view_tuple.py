be9a264e77c1721de9f9848a88acf356ff1cc33b
Taylor Holliday <taylor@audulus.com>
2023-06-16 07:52:00 -0700

    re #50 Add foreach_view_rev

--- src/gen_view_tuple.py
+++ src/gen_view_tuple.py
@@ -9,8 +9,10 @@ for i in range(1, max_elements+1):
     print("impl_view_tuple!(%d; " % i, end="")
     for j in range(i-1):
         print("V%d, " % j, end="")
-    print("V%d" % (i-1), end="")
-    print("; ", end="")
+    print("V%d; " % (i-1), end="")
     for j in range(i-1):
         print("%d, " % j, end="")
-    print("%d);" % (i-1))
+    print("%d; " % (i-1), end="")
+    for j in reversed(range(1, i)):
+        print("%d, " % j, end="")
+    print("0);")
