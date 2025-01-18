821cb9d436d91840f942969460928dc5bde8a129
Kharif <rydenfalk.service@gmail.com>
2025-01-18 16:46:52 +0100

    animated toggle

--- src/views/toggle.rs
+++ src/views/toggle.rs
@@ -1,26 +1,39 @@
 use crate::*;
 
-/// Toggle switch.
+/// A toggle switch.
 pub fn toggle(on: impl Binding<bool>) -> impl View {
+    let width = 35.0f32;
+    let edge = 1.0f32;
+    let double_edge = edge * 2.0;
+    let height = 20.0f32;
+
     state(
-        || (),
-        move |_, cx| {
+        || 0.0f32,
+        move |animation: StateHandle<f32>, cx| {
             let b = *on.get(cx);
             zstack((
                 rectangle()
                     .color(if b {
-                        AZURE_HIGHLIGHT_BACKGROUND
+                        AZURE_HIGHLIGHT
                     } else {
                         CONTROL_BACKGROUND
                     })
                     .corner_radius(10.0)
-                    .size([40.0, 20.0])
+                    .size([width, 20.0])
                     .tap(move |cx| on.with_mut(cx, |b| *b = !*b)),
                 circle()
-                    .color(if b { AZURE_HIGHLIGHT } else { MEDIUM_GRAY })
-                    .size([10.0, 10.0])
-                    .offset([if b { 25.0 } else { 5.0 }, 5.0]),
+                    .color(MEDIUM_GRAY)
+                    .size([height - double_edge, height - double_edge])
+                    .offset([cx[animation] * (width - height) + edge, edge]),
             ))
+            .anim(move |cx, dt| {
+                let target = if b { 1.0 } else { 0.0 };
+                let delta = target - cx[animation];
+                cx[animation] += delta * 15.0 * dt;
+                if delta.abs() < 0.01 {
+                    cx[animation] = target;
+                }
+            })
         },
     )
 }
