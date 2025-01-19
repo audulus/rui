9313a3dac18d15cd50aad9dba89a9d2f088624e9
Taylor Holliday <wtholliday@gmail.com>
2025-01-18 21:56:37 -0800

    #53 add StateHolder2

--- src/state_holder.rs
+++ src/state_holder.rs
@@ -1,6 +1,23 @@
 use std::any::Any;
+use std::rc::Rc;
+use std::cell::{Ref, RefCell};
 
 pub(crate) struct StateHolder {
     pub state: Box<dyn Any>,
     pub dirty: bool,
+}
+
+pub(crate) struct StateHolder2 {
+    pub state: Rc<dyn Any>,
+    pub dirty: bool,
+}
+
+impl StateHolder2 {
+    pub fn borrow<T: 'static>(&self) -> Ref<'_, T> {
+        self
+            .state
+            .downcast_ref::<RefCell<T>>()
+            .unwrap()
+            .borrow()
+    }
 }
