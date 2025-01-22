3e84867a62095c89944c0baaed8a9dbe666bb9a0
Taylor Holliday <wtholliday@gmail.com>
2025-01-22 09:56:43 -0800

    #72 add Clone

--- src/views/modview.rs
+++ src/views/modview.rs
@@ -85,7 +85,7 @@ where
 }
 
 /// Passes a value to a function. Value can be updated by modifiers.
-pub fn modview<S: Clone + Default + 'static, V: View, F: Fn(S, &mut Context) -> V + 'static>(
+pub fn modview<S: Clone + Default + 'static, V: View, F: Fn(S, &mut Context) -> V + Clone + 'static>(
     f: F,
 ) -> ModView<S, F> {
     ModView {
