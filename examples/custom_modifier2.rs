12f64c674677fda0b72d2335072f7b260d367b94
Taylor Holliday <wtholliday@gmail.com>
2025-01-22 00:02:36 -0800

    #72 stateful modifier

--- examples/custom_modifier2.rs
+++ examples/custom_modifier2.rs
@@ -1,14 +1,29 @@
 use rui::*;
 
-trait MyMod: View + Sized {
-    fn my_modifier(self) -> impl View {
-        self.offset(LocalOffset::new(0.0, 100.0))
+struct MyState {
+    offset: f32,
+}
+
+impl Default for MyState {
+    fn default() -> Self {
+        MyState { offset: 100.0 }
     }
 }
 
-impl<V: View> MyMod for V {}
+trait MyMod: View + Clone + Sized {
+    fn my_modifier(self) -> impl View {
+        state(MyState::default, move |s, cx| {
+            self.clone().offset(LocalOffset::new(0.0, cx[s].offset))
+                .anim(move |cx, _| {
+                    cx[s].offset *= 0.9;
+                })
+        })
+    }
+}
 
-fn my_text(name: &str) -> impl View {
+impl<V: View + Clone> MyMod for V {}
+
+fn my_text(name: &str) -> impl View + Clone {
     let name = name.to_string();
     text(name.as_str())
 }
